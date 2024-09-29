cc420da98fbc5cfa7d1bd0d467c97fcf336f3655
Shreya <shreyagoel098@gmail.com>
2024-09-30 00:47:12 +0530

    Added more animation for other info

--- src/utils.tsx
+++ src/utils.tsx
@@ -5,16 +5,16 @@ export const imageInfo = [
     className: "heading",
   },
   {
-    transform: "scale(1)",
+    transform: "scale(2)",
     info: "It's a lush life. Beyond the horizon the empire stirs; here we all dressed up, in extravagant soltitude",
     fontWeight: "500",
   },
   {
-    transform: "scale(6.66667, 6.66667) translate(15.5%, -0.5%)",
+    transform: "scale(5) translate(15.5%, -0.5%)",
     info: "We’ve come to the north of India. It feels like summer; flat-bottomed boats ply the Yamuna River.",
   },
   {
-    transform: "scale(6.66667) translate(-26.5%, 28.5%)",
+    transform: "scale(5) translate(-26.5%, 20%)",
     info: "A few small birds flutter through the dry air.Also flying creatures of a more celestial order.",
   },
   {
@@ -38,7 +38,7 @@ export const imageInfo = [
     info: "He commissioned this one right around the time he took the throne, at 36.",
   },
   {
-    transform: "scale(1.5) translate(-7.5%, -31.5%)",
+    transform: "scale(3) translate(-10.5%, -31.5%)",
     info: "It was painted by an artist called Chitarman, who was the head of the imperial atelier. He was a master of the Mughal style, which was a blend of Persian and Indian traditions.",
   },
   {
@@ -54,43 +54,43 @@ export const imageInfo = [
     info: "You will not, however, find it with the bulk of the museum's Indian art. This painting, and the alburn it comes from, are in the Islamic collection; it's a Muslim masterpiece made 2.500 miles from Mecca.",
   },
   {
-    transform: "scale(4)",
+    transform: "scale(2.9) translate(-5.5%,5.5%)",
     info: "Mughal miniatures are unbelievably detailed, but that's not what I love most about them.",
   },
   {
-    transform: "scale(1)",
+    transform: "scale(3.5) translate(10%,23.5%)",
     info: "What I love is their profound cosmmopolitians:Hindu, Islamic and even Christian motifs, blended into images of the heighest prestige.",
   },
   {
-    transform: "scale(1)",
+    transform: "scale(3.5) translate(0%,-10.5%)",
     info: "Within the details of this miniature lies a master class in the political uses of cultural hybridity. And the uses of something else too:  dumbfolding, superhuman beauty.",
   },
   {
-    transform: "scale(1)",
+    transform: "scale(1.5)",
     info: "Chitarman has painted the new emperor as an icon of stability. His pose is formal, stiff. The depiction in profile gives him the bearing of someone standing apart from the world.",
   },
   {
-    transform: "scale(1)",
+    transform: "scale(4.5) translate(-5.5%, 6%)",
     info: "His chest is garlanded with jewels: strings of fat perals interrupted by rubies and emeralds. And advertisment of wealth, though scholars have observed religious significance. A young Muslim man would waer strands like at his wedding.",
   },
   {
-    transform: "scale(1)",
-    infp: "Shah jahan is wearing a weighty pink coat, called a jama, festooned with flowers, belted with sash of gold and orange. The coat is tied to the right, in Muslim fashion.(Hindus tied their to the left.)",
+    transform: "scale(5) translate(-5.5%, 0%)",
+    info: "Shah jahan is wearing a weighty pink coat, called a jama, festooned with flowers, belted with sash of gold and orange. The coat is tied to the right, in Muslim fashion.(Hindus tied their to the left.)",
   },
   {
-    transform: "scale(1)",
+    transform: "scale(5) translate(-5.5%, 9%)",
     info: "On his head is a purple turban, stung with more jewels and topped with peacock features. That bird was a emblem:In the year this was painted, Shah Jahan ordered up the stupefyingly ornate Peacock Throne, whose jewels are now dispersed.",
   },
   {
-    tranform: "scale(1)",
+    transform: "scale(3) translate(-5.5%, -15%)",
     info: "Behind him is a marble scree, called a jali: an archetype of Indo-Persian luxury. The screen, as well as the floral-patterned brocade draped over its center, would have been made in one of the many imperial workshops.",
   },
   {
-    transform: "scale(1)",
+    transform: "scale(4) translate(-5.5%, -15%)",
     info: "A painting like this would also have been the product of many hands. Chitarman would have designed the compostion, selected the colors; then apprentices did the painstaking work, with fine brushes of squirrel hair.",
   },
   {
-    transform: "scale(1)",
+    transform: "scale(1.5)",
     info: "But this was court art, and the emperor tightly controlled his artists' production. He ordered them to depict him in profile only ans as buffed and radiant as those emeralds and rubies.",
   },
   {
